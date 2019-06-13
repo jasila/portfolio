829d34eda0050d34111afe5d1c2e7e02cbc808b0
Jenny Sila <jennysila@laptop-5vi6sman.kea.local>
2019-06-13 16:32:01 +0200

    noget rod, fix imorgen jenny, js lytter ikke og rod med parent og childs

---
+++ script.js
@@ -0,0 +1,29 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+
+
+
+}
+
+function toggleMenu() {
+    console.log("toggleMenu");
+
+    //hvordan er toggle ift. add og remove??? HELP
+    document.querySelector("#menu").classList.toggle("hidden");
+
+    // let erskjult er lig med navmuligheder når den indeholder class'en hidden
+    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+    // hvis navmuligheder er skjult, så skal man se en burgermenu
+    if (erSkjult == true) {
+        document.getElementById("menuknap").src = "startside/ui/menu.svg"
+    }
+
+    //hvis i navmuligheder er skjult skal man se et kryds
+    else {
+        document.getElementById("menuknap").src = "startside/ui/menu.svg";
+    }
+}
